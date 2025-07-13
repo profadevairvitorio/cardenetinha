f8761230ae638c378e8c1da099323d9ea0af375d
adevair.junior <profadevairvitorio@gmail.com>
2025-07-13 16:11:41 -0300

    feat: adds forms for account management and transactions

--- app/forms.py
+++ app/forms.py
@@ -1,17 +1,15 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import DataRequired, Email, EqualTo, Length, ValidationError
-from .models import User
+from wtforms import StringField, PasswordField, BooleanField, SubmitField, SelectField, DecimalField
+from wtforms.validators import DataRequired, Email, EqualTo, Length, ValidationError, Optional, NumberRange
+from .models import User, Account
 
 class LoginForm(FlaskForm):
-    """Formulário de Login"""
     email = StringField('Email', validators=[DataRequired(), Email()])
     password = PasswordField('Senha', validators=[DataRequired()])
     remember_me = BooleanField('Lembrar-me')
     submit = SubmitField('Entrar')
 
 class RegistrationForm(FlaskForm):
-    """Formulário de Registro"""
     username = StringField('Nome de Usuário', validators=[DataRequired(), Length(min=4, max=25)])
     email = StringField('Email', validators=[DataRequired(), Email()])
     password = PasswordField('Senha', validators=[DataRequired(), Length(min=6)])
@@ -27,3 +25,41 @@ class RegistrationForm(FlaskForm):
         user = User.query.filter_by(email=email.data).first()
         if user:
             raise ValidationError('Este email já está registrado. Por favor, use outro.')
+
+
+class AccountForm(FlaskForm):
+    name_account = StringField(
+        'Nome da Conta',
+        validators=[
+            DataRequired(message="O nome da conta é obrigatório."),
+            Length(min=3, max=100, message="O nome deve ter entre 3 e 100 caracteres.")
+        ]
+    )
+
+    initial_balance = DecimalField(
+        'Saldo Inicial (Opcional)',
+        places=2,
+        validators=[
+            Optional(),
+            NumberRange(min=0, message="O saldo inicial não pode ser negativo.")
+        ],
+        default=0.00
+    )
+
+    submit = SubmitField('Criar Conta')
+
+class EditAccountForm(FlaskForm):
+    name_account = StringField(
+        'Nome da Conta',
+        validators=[
+            DataRequired(message="O nome da conta é obrigatório."),
+            Length(min=3, max=100, message="O nome deve ter entre 3 e 100 caracteres.")
+        ]
+    )
+    submit = SubmitField('Salvar Alterações')
+
+class TransactionForm(FlaskForm):
+    description = StringField('Descrição', validators=[DataRequired(), Length(max=200)])
+    amount = DecimalField('Valor', places=2, validators=[DataRequired(), NumberRange(min=0.01)])
+    type = SelectField('Tipo', choices=[('entrada', 'Entrada'), ('saida', 'Saída')], validators=[DataRequired()])
+    submit = SubmitField('Registrar Transação')
