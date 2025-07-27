5569f7309a46a8c5cfd871e146e060059d43442c
profadevairvitorio <profadevairvitorio@gmail.com>
2025-07-27 14:39:31 -0300

    feat: Use environment variables for application configuration in run script and .env example

--- run.py
+++ run.py
@@ -1,6 +1,11 @@
+import os
 from app import create_app
 
 app = create_app()
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5050, debug=True)
+    app.run(
+        host=os.environ.get('APPLICATION_HOST'),
+        port=os.environ.get('APPLICATION_PORT'),
+        debug=os.environ.get('APPLICATION_DEBUG')
+    )
