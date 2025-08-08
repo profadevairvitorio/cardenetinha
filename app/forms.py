15bb8edb450de78239e74d66932e80181881ddca
profadevairvitorio <profadevairvitorio@gmail.com>
2025-08-07 22:36:58 -0300

    feat: Add FinancialPlanForm with category-specific initialization

--- app/forms.py
+++ app/forms.py
@@ -153,3 +153,17 @@ class GoalForm(FlaskForm):
         validators=[DataRequired()]
     )
     submit = SubmitField('Salvar Meta')
+
+class FinancialPlanForm(FlaskForm):
+    description = StringField('Descrição', validators=[DataRequired(), Length(max=200)])
+    amount = DecimalField('Valor', places=2, validators=[DataRequired(), NumberRange(min=0.01)])
+    type = SelectField('Tipo', choices=[('entrada', 'Entrada'), ('despesa', 'Despesa')], validators=[DataRequired()])
+    plan_date = DateField('Data do Planejamento', format='%Y-%m-%d', validators=[DataRequired()])
+    category_id = SelectField('Categoria', coerce=int, validators=[DataRequired(message="Selecione uma categoria.")])
+    submit = SubmitField('Adicionar ao Planejamento')
+
+    def __init__(self, *args, **kwargs):
+        super(FinancialPlanForm, self).__init__(*args, **kwargs)
+        if 'user' in kwargs:
+            user = kwargs['user']
+            self.category_id.choices = [(c.id, c.name) for c in Category.query.filter_by(user_id=user.id).order_by(Category.name).all()]
